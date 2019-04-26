5e8abe3b07cf60c3499b5a14fbeca24b9ef2a4c7
Prakash Chenga <prakash.chenga@gmail.com>
2019-04-26 18:48:54 +0530

    'update'

--- src/com/pc/sparkcore/RDDToDataFrame.scala
+++ src/com/pc/sparkcore/RDDToDataFrame.scala
@@ -2,6 +2,7 @@ package com.pc.sparkcore
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
 import org.apache.spark.rdd.RDD
 
 
@@ -23,7 +24,9 @@ object RDDToDataFrame {
                             
    
    import spark.implicits._                         
-   //RDD --core SparkContext  
+   //RDD --core SparkContext 
+   
+   spark.sparkContext.setLogLevel("OFF")
                             
     val inputRDD:RDD[String] = spark.sparkContext.textFile("E:/Scala_Durga/Vamshi_PIO_Support/MOCK_DATA.csv")                       
     
@@ -33,13 +36,83 @@ object RDDToDataFrame {
      User(cols(0),cols(1),cols(2))
     }
     )
+    /*
+    RDD[User,  val user = User(1,p1,1)
+    User,
+    User,
+    User...] */
+    userRDD.foreach(user => println(s"userId = ${user.userId} productId =${user.productId}") )
     
     val userDF = userRDD.toDF()
-    userDF.printSchema()
-    userDF.show()
+    //userDF.printSchema()
+    //userDF.show()
+    
+    
+    //DataFrame = RDD[Row] + User(userId:String, productId:String, visitorId:String)
+    //1. SQL API
+    
+    userDF.createOrReplaceTempView("user_table")
+    
+    userDF.as("u")
+    
+    val df1 = spark.sql("SELECT userId,productId FROM user_table")
+    df1.printSchema()
+    df1.show()
+    
+    /*
+    RDD[Row,
+    Row,
+    Row...]
+    
+    Row[col0,col1...] getXXX getSting
+    
+    */
+    
+    df1.foreach(r => println(r.getString(0) +"\t" + r.getString(1)))
+        
+    //2. DataFrame/DSL API  Column, Column 
+    userDF.select("userId","visitorId").show()
+    userDF.select(userDF.col("userId"),userDF.col("visitorId")).show()
+    userDF.select(
+          $"userId",
+          $"productId",
+          $"visitorId"
+        ).show()
+        
+        
+    //ProductDF
+    val products = Seq(
+        ("p1","Laptop"),
+        ("p2","TV"),
+        ("p3","Projector")
+        )  
+     
+   val productDF = products.toDF("productId","productName")     
+   
+   productDF.printSchema
+   productDF.show
+   
+   
+   val finalDF = userDF.join(productDF,userDF.col("productId") === productDF.col("productId") ,"left_outer")
+   
+   finalDF.printSchema()
+   finalDF.show()
+   
+   
+ val resDF=
+       finalDF.select(
+       userDF.col("userId"),  
+       userDF.col("visitorId"),   
+       productDF.col("productId"),
+       when(productDF.col("productName").isNull, "NA").otherwise(productDF.col("productName")).as("productDes")
+       )
+       
+  resDF.printSchema
+  resDF.show
+   
+        
      
     
-    //DataFrame 
     
   }
 }
